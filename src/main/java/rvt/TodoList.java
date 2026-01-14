ac0d093a97d42db09be5c70ec85662e3da724754
24DP1PBerz <a240332pb@rvt.lv>
2026-01-14 16:01:59 +0200

    To do list (nepabeigts)

---
+++ src/main/java/rvt/TodoList.java
@@ -0,0 +1,48 @@
+package rvt;
+
+import java.util.ArrayList;
+
+public class TodoList {
+    ArrayList<String> tasks;
+
+    public TodoList(){
+        tasks = new ArrayList<>();
+    }
+
+    public void add(String task){
+        
+        tasks.add(task);
+    }
+    public void print(){
+        for(int i = 0; i < tasks.size(); i++){
+            System.out.println((i+1) + ": "+ tasks.get(i));
+        }
+    }
+    public void remove(int number){
+        tasks.remove(number - 1);
+    }
+
+    public static void main(String[] args) {
+        
+        TodoList list = new TodoList();
+        list.add("read the course material");
+        list.add("watch the latest fool us");
+        list.add("take it easy");
+
+        list.print();
+        list.remove(2);
+
+        System.out.println();
+        list.print();
+    }
+}
+
+class UserInterface{
+
+    public void start(){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
