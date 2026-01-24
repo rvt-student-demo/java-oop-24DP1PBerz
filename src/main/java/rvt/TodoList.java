14d6bcb9ac1935b68de16390887904d3a3dfab3f
24DP1PBerz <a240332pb@rvt.lv>
2026-01-24 19:34:03 +0200

    To do list papildus

--- src/main/java/rvt/TodoList.java
+++ src/main/java/rvt/TodoList.java
@@ -1,24 +1,79 @@
 package rvt;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class TodoList {
-    ArrayList<String> tasks;
+    private ArrayList<String> tasks;
+    private final String filePath="data/todo.csv";
 
     public TodoList(){
-        tasks = new ArrayList<>();
+        this.tasks = new ArrayList<>();
+        loadFromFile();
+    }
+    
+    private void loadFromFile(){
+        try(BufferedReader br = new BufferedReader(new FileReader("data/todo.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                this.tasks.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
     }
 
+    private int getLastId(){
+            return this.tasks.size() - 1;
+    }
     public void add(String task){
-        
-        tasks.add(task);
+        int nextId = getLastId()+1;
+        this.tasks.add(nextId + "," + task);
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("data/todo.csv", true))) {
+            bw.write(nextId + "," + task);
+            bw.newLine();
+        } catch(IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
     }
     public void print(){
         for(int i = 0; i < tasks.size(); i++){
-            System.out.println((i+1) + ": "+ tasks.get(i));
+        System.out.println(tasks.get(i));
         }
     }
-    public void remove(int number){
-        tasks.remove(number - 1);
+
+    private boolean updateFile(){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("data/todo.csv"))) {
+            for(int i = 0; i < tasks.size();i++){
+            bw.write(tasks.get(i).toString());
+            bw.newLine();
+            
+            }
+            return true;
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+    }
+    public void remove(int id){
+        this.tasks.remove(id);
+        updateFile();
+    }
+
+    public static void main(String[] args) {
+        TodoList todoList = new TodoList();
+        todoList.add("Buy groceries");
+        todoList.add("wash the dishes");
+        todoList.print();
+        System.out.println("Last ID: " + todoList.getLastId());
+        todoList.remove(5);
+        System.out.println();
+        todoList.print();
     }
 }
