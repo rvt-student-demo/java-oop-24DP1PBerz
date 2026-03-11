e37333de258e24badc0376c4e176484f89189eee
24DP1PBerz <a240332pb@rvt.lv>
2026-03-11 11:09:24 +0200

    I owe you

---
+++ src/main/java/rvt/IOU.java
@@ -0,0 +1,32 @@
+package rvt;
+
+import java.util.HashMap;
+
+public class IOU {
+    private HashMap<String, Double> debts;
+
+    public IOU(){
+        this.debts = new HashMap<>();
+    }
+
+    public void setSum(String toWhom, double amount){
+        debts.put(toWhom,amount);
+    }
+
+    public double howMuchDoIOweTo(String toWhom){
+        if(debts.containsKey(toWhom)){
+            return debts.get(toWhom);
+        }else{
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        IOU mattsIOU = new IOU();
+        mattsIOU.setSum("Arthur", 51.5);
+        mattsIOU.setSum("Michael", 30);
+
+        System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));
+        System.out.println(mattsIOU.howMuchDoIOweTo("Michael"));
+    }
+}
