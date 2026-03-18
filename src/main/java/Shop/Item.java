2e9f85cbeba06d9d85d37c9c6dfcc7395bc3cc90
24DP1PBerz <a240332pb@rvt.lv>
2026-03-18 11:15:37 +0200

    Online shop (nepabeigts)

---
+++ src/main/java/Shop/Item.java
@@ -0,0 +1,15 @@
+package Shop;
+
+public class Item {
+    public String product;
+    public int qty;
+    public int unitPrice;
+
+    public Item(String product, int qty, int unitPrice){
+        this.product = product;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+
+}
---
+++ src/main/java/Shop/Warehouse.java
@@ -0,0 +1,57 @@
+package Shop;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+
+public class Warehouse {
+    Map<String, Integer> products = new HashMap<>();
+
+    public void addProduct(String product, int price, int stock){
+        products.put(product, price);
+        products.put(product, stock);
+    }
+
+    public int price(String product){
+        if(products.containsKey(product)){
+            return products.get(product);
+        }else{
+            return -99;
+        }
+        
+    }
+
+    public int stock(String product){
+        if(products.containsKey(product)){
+            return products.get(product);
+        }else{
+            return 0;
+        }
+    }
+
+    public boolean take(String product){
+        if(products.containsKey(product) && products.get(product) > 0){
+            products.put(product, products.get(product) - 1);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public Set<String> products(){
+        return products.keySet();
+    }
+
+    public static void main(String[] args) {
+       Warehouse warehouse = new Warehouse();
+        warehouse.addProduct("milk", 3, 10);
+        warehouse.addProduct("coffee", 5, 6);
+        warehouse.addProduct("buttermilk", 2, 20);
+        warehouse.addProduct("yogurt", 2, 20);
+
+        System.out.println("products:");
+
+        for (String product: warehouse.products()) {
+            System.out.println(product);
+        }   
+    }
+}
