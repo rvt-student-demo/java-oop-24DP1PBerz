5a52ae6a061f9e2eb01c2d0532ec42a3f1c1c427
24DP1PBerz <a240332pb@rvt.lv>
2026-06-05 23:31:49 +0300

    Pievienots  TodoUI label

--- src/main/java/rvt/TodoUI.java
+++ src/main/java/rvt/TodoUI.java
@@ -3,16 +3,28 @@ package rvt;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 //import java.util.Scanner;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.JLabel;
+
+import lombok.Builder.ObtainVia;
+
 import javax.swing.JButton;
 
 public class TodoUI {
     private JFrame window;
     private JPanel panel;
     private JButton button;
+    private JLabel label;
 
     public TodoUI(){
         initialize();
@@ -34,6 +46,21 @@ public class TodoUI {
         button = createbutton("Add Task", "Add a new task into the list");
         panel.add(button);
 
+        JPanel panel2;
+
+        panel2 = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 5));
+        panel2.setBackground(new Color(69, 4, 4));
+        label = new JLabel("TODO");
+        //window.add(label);
+        //window.add(label, BorderLayout.NORTH);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.BOTTOM);
+        label.setForeground(Color.cyan);
+        label.setFont(new Font("Times New Roman",Font.BOLD,36));
+        panel2.add(label);
+        window.add(panel2,BorderLayout.NORTH);
+
+
     }
 
 
@@ -44,6 +71,14 @@ public class TodoUI {
     private JButton createbutton(String X, String Y){
         JButton button = new JButton(X);
         button.setToolTipText(Y);
+        button.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        button.setMargin(new Insets(10, 10, 10, 10));
+        button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                System.out.println("button clicked");
+            }
+        });
         return button;
     }
     // TodoList todo;
